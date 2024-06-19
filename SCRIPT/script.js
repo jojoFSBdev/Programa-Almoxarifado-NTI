c3a4010472f6175a6e592e375c332b31997d5299
jojoFSB.dev <joaquimfsb.si@gmail.com>
2024-06-19 17:55:56 -0300

    teste

---
+++ SCRIPT/script.js
@@ -0,0 +1,13 @@
+
+
+function AbrirMenu() {
+    var inputEmail = document.getElementById("Input_Email").value;
+    var inputPass = document.getElementById("Input_Pass").value;
+    
+    console.log(inputEmail, inputPass)
+    if (inputEmail != "teste" || inputPass != "123321") {
+        window.alert("Email ou Senha inseridos esta incorreto!")
+    }else{
+        window.location.href = "../HTML/tela_Principal.html"
+    }
+}
