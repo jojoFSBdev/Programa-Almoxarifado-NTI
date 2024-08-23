eed776e2a745f9f1ecb96b370aaad7721e1cb674
jojoFSB.dev <joaquimfsb.si@gmail.com>
2024-08-23 14:35:43 -0300

    ss

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,13 @@
 
-function openMenu() {
-    var emailElement = document.querySelector(".INPT_email").value;
-    var senhaElement = document.querySelector(".INPT_pass").value;
+function acessar() {
+    var emailElement = document.getElementById("INPT_Email").value;
+    var senhaElement = document.getElementById("INPT_Pass").value;
     
     console.log(emailElement, senhaElement);
+
+    if (emailElement != "teste" || senhaElement != "123321") {
+        window.alert("Email ou Senha estão incorretas!");
+    } else {
+        window.open("../HTML/tela_Principal.html", "_self");
+    }
 }
